2a3eb6814b031ed3b7976cd7b7b719c5a6d72c72
50 Дибчук Дибчук <102529857+enochtheguardian@users.noreply.github.com>
2022-03-28 15:27:52 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+
+int main()
+
+{ 
+    int a[15], i, k=0; int S=0; int n;
+   
+  cout<<"Введiть кількість елементів масиву (до 15):"; 
+  cin >> n;    
+  if (n>15) n=15;
+  
+  cout<<"Введiть 15 елементів масиву:\n"; 
+   for (i=0; i<15; i++)
+
+  {  cin >> a[i];
+
+     if  (a[i]%2 == 0)  {k++; S+=a[i];}
+
+  }
+
+  cout << "Кількість парних елементів у масиві = " << k << endl;
+  cout << "Cума парних елементів у масиві = " << S << endl;
+
+
 }
+
+
